452618e478b792fea452f5cb08a4b4e5760a0b6b
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-08-08 13:51:18 +0200

    step 2 bookstore

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -5,15 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String title;
