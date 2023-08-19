1e786bcc31b9dee809c2fdf6b98b657806cca80e
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-08-19 23:34:13 +0200

    commit hw

--- src/main/java/mate/academy/bookstore/controller/BookController.java
+++ src/main/java/mate/academy/bookstore/controller/BookController.java
@@ -3,13 +3,18 @@ package mate.academy.bookstore.controller;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.model.dto.BookDto;
+import mate.academy.bookstore.model.dto.BookSearchParametersDto;
 import mate.academy.bookstore.model.dto.CreateBookRequestDto;
 import mate.academy.bookstore.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -31,6 +36,23 @@ public class BookController {
     @PostMapping("/ctreate_book")
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable Long id,
+                              @RequestBody CreateBookRequestDto bookDto) {
+        return bookService.updateById(id, bookDto);
+    }
+
+    @GetMapping("/search")
+    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters) {
+        return bookService.search(searchParameters);
     }
 
 }
---
+++ src/main/java/mate/academy/bookstore/model/dto/BookSearchParametersDto.java
@@ -0,0 +1,4 @@
+package mate.academy.bookstore.model.dto;
+
+public record BookSearchParametersDto(String[] titles, String[] authors) {
+}
