a7e82f8f08c4ce98995ccf00ffb0a48bd43fd5d1
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-08-23 09:48:47 +0200

    add a description in swagger to each endpoint

--- src/main/java/mate/academy/bookstore/controller/BookController.java
+++ src/main/java/mate/academy/bookstore/controller/BookController.java
@@ -27,34 +27,46 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     private final BookService bookService;
 
+    @Tag(name = "Get all books",
+            description = "This endpoint returns a list of books.")
     @GetMapping
     public List<BookDto> findAll(Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Tag(name = "Get books by ID",
+            description = "This endpoint returns a book by id")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
     @PostMapping("/ctreate_book")
+    @Tag(name = "Create new book",
+            description = "This endpoint create a book")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping ("/{id}")
+    @Tag(name = "Delete book by ID",
+            description = "This endpoint mark book field is_delete = true")
     public void delete(@PathVariable Long id) {
         bookService.deleteById(id);
     }
 
     @PutMapping("/{id}")
+    @Tag(name = "Update book by ID",
+            description = "This endpoint returns book with new data")
     public BookDto updateBook(@PathVariable Long id,
                                @RequestBody CreateBookRequestDto bookDto) {
         return bookService.updateById(id, bookDto);
     }
 
     @GetMapping("/search")
+    @Tag(name = "Get a list of books",
+            description = "returns a list of books according to searching params")
     public List<BookDto> searchBooks(BookSearchParametersDto searchParameters) {
         return bookService.search(searchParameters);
     }
