c6725d6bfa0411351cee42201d72d89364ee9ff1
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-09-28 16:30:54 +0200

    BookStoreApplicationTests correction

--- src/test/java/mate/academy/bookstore/BookStoreApplicationTests.java
+++ src/test/java/mate/academy/bookstore/BookStoreApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+        (classes = BookStoreApplicationTests.class)
 class BookStoreApplicationTests {
 
     @ClassRule
