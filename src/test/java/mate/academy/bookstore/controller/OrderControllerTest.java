dd9b60698de5f0062dcb566bf0dacf4bc38424ff
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-10-09 21:15:27 +0200

    orderController Test

--- src/test/java/mate/academy/bookstore/controller/OrderControllerTest.java
+++ src/test/java/mate/academy/bookstore/controller/OrderControllerTest.java
@@ -1,37 +1,54 @@
 package mate.academy.bookstore.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
-import mate.academy.bookstore.repository.book.BookRepository;
+import mate.academy.bookstore.model.User;
+import mate.academy.bookstore.model.dto.order.OrderDto;
+import mate.academy.bookstore.model.dto.order.OrderItemDto;
+import mate.academy.bookstore.model.dto.order.OrderRequestDto;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class OrderControllerTest {
 
     @Autowired
-    protected static MockMvc mockMvc;
+    protected MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
-
-    @MockBean
-    BookRepository bookRepository;
 
     @BeforeEach
     void beforeAll(
@@ -50,6 +67,11 @@ class OrderControllerTest {
                     new ClassPathResource("database/sql-shopping-cart/controller"
                             + "/add-data.sql")
             );
+            ScriptUtils.executeSqlScript(
+                    connection,
+                    new ClassPathResource("database/sql-order/controller"
+                            + "/add-order.sql")
+            );
         }
     }
 
@@ -66,32 +88,108 @@ class OrderControllerTest {
                     connection,
                     new ClassPathResource("database/sql-shopping-cart/controller/delete-data.sql")
             );
+            ScriptUtils.executeSqlScript(
+                    connection,
+                    new ClassPathResource("database/sql-order/controller"
+                            + "/delete-order.sql")
+            );
         }
     }
 
-
-
     @Test
-    void addOrder() {
+    void createOrder_ReturnOrderDto() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("user3@example.com");
+
+        OrderRequestDto requestDto = new OrderRequestDto();
+        requestDto.setShippingAddress("test address");
+
+        Authentication authentication = getAuthorisation(user);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(requestDto);
+
+        OrderDto response = getOrderByUser(user);
+
+        MvcResult result = mockMvc.perform(
+                        post("/orders")
+                                .with(authentication(authentication))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestJson)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        OrderDto actual = objectMapper.readValue(result.getResponse().getContentAsString(),
+                OrderDto.class);
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(response.getOrderItems().size(), actual.getOrderItems().size());
     }
 
     @Test
-    void getOrderHistory() {
+    @DisplayName("test method getOrderHistory return list of orders")
+    void getOrderHistory_ReturnOk() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("user3@example.com");
+
+        Authentication authentication = getAuthorisation(user);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        OrderDto response = getOrderByUser(user);
+        List<OrderDto> responsesList = new ArrayList<>();
+        responsesList.add(response);
+
+        MvcResult result = mockMvc.perform(get("/orders")
+                        .with(authentication(authentication))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<OrderDto> actual = objectMapper.readValue(
+                result.getResponse().getContentAsString(),
+                objectMapper.getTypeFactory().constructCollectionType(List.class, OrderDto.class)
+        );
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(responsesList.size(), actual.size());
     }
 
-    @Test
-    void findItemsByOrderId() {
+    private Authentication getAuthorisation(User user) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return new UsernamePasswordAuthenticationToken(
+                user, "password",authorities);
     }
 
-    @Test
-    void getOrderItemByItemId() {
-    }
+    @NotNull
+    private static OrderDto getOrderByUser(User user) {
+        OrderItemDto orderItemDto1 = new OrderItemDto();
+        orderItemDto1.setId(1L);
+        orderItemDto1.setBookId(1L);
+        orderItemDto1.setQuantity(10);
 
-    @Test
-    void updateOrderStatus() {
-    }
+        OrderItemDto orderItemDto2 = new OrderItemDto();
+        orderItemDto2.setId(2L);
+        orderItemDto2.setBookId(2L);
+        orderItemDto2.setQuantity(15);
 
-    @Test
-    void getUserByAuth() {
+        OrderItemDto orderItemDto3 = new OrderItemDto();
+        orderItemDto3.setId(3L);
+        orderItemDto3.setBookId(3L);
+        orderItemDto3.setQuantity(10);
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setOrderItems(List.of(orderItemDto1, orderItemDto2, orderItemDto3));
+        orderDto.setUserId(user.getId());
+        orderDto.setId(1L);
+        return orderDto;
     }
 }
---
+++ src/test/resources/database/sql-order/controller/add-order.sql
@@ -0,0 +1,3 @@
+INSERT INTO orders (id, user_id, status, total, order_date, shipping_address)
+values (1, 1, 'NEW', 720, '2023-09-10', 'test address');
+
